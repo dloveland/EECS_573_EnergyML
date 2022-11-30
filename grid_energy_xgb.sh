a187fdfb4ef53152ec692c06b47c75e5c15a025e
salamentic <anrao3@buffalo.edu>
2022-11-30 03:44:11 -0500

    Added script for xgb, cleaned one for rf; TODO: add perf when on linux boot

--- grid_energy_xgb.sh
+++ grid_energy_xgb.sh
@@ -3,23 +3,24 @@
 for dataset in "bank" "maternal" "winequality"; do
 	EXP_NUM=0
 	if [[ $dataset == "winequality" ]]; then
-		echo "Num,Max Depth,n_estimators,Max Features,MSE,MAE" > search_results_rf_$dataset.csv
+		echo "Num,Max Depth,n_estimators,Max Features,MSE,MAE" > search_results_xgb_$dataset.csv
 	else
-		echo "Num,Max Depth,n_estimators,Max Features,Accuracy,Precision,Recall,F1 Score,Micro F1" > search_results_rf_$dataset.csv
+		echo "Num,Max Depth,n_estimators,Learning Rate,Accuracy,Precision,Recall,F1 Score,Micro F1" > search_results_xgb_$dataset.csv
 	fi
+
 	for max_d in 3 4 5; do
 		for n_estim in 50 100 150; do
-			for max_feat in 0.25 0.5 1; do
+			for lr in 0.1 0.01 0.001; do
 				echo "P_MAX_D=$max_d" > hyperparams.py
 				echo "P_N_ESTIM=$n_estim" >> hyperparams.py
-				echo "P_MAX_FEAT=$max_feat" >> hyperparams.py
+				echo "P_LR=$lr" >> hyperparams.py
 				echo "EXP_NAME=$EXP_NUM" >> hyperparams.py
 				# Just to fix import issues, ignore
-				echo "P_LR=None" >> hyperparams.py
+				echo "P_MAX_FEAT=99999" >> hyperparams.py
 
 				# Do everything for perf, change this to writerow or redirect output to file
-				# perf stat -e "power/energy" -x ',' python3 run_ml_model.py --model rf --data $dataset --notune >> energy_rf_$dataset.csv
-				python3 run_ml_model.py --model rf --data $dataset --notune >> search_results_rf_$dataset.csv
+				# perf stat -e "power/energy" -x ',' python3 run_ml_model.py --model xgb --data bank --notune >> energy_xgb_bank.csv
+				python3 run_ml_model.py --model xgb --data $dataset --notune >> search_results_xgb_$dataset.csv
 				EXP_NUM=$((EXP_NUM+1))
 			done
 		done
